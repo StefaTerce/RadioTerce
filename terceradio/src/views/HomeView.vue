45a0f6105ae9ecc4aa4661547aae17a5cecef5f8
Stefano Terceros <tercerosstefano@gmail.com>
2024-04-30 11:48:17 +0000

    UpdateVue

--- terceradio/src/views/HomeView.vue
+++ terceradio/src/views/HomeView.vue
@@ -1,70 +1,64 @@
 <template>
- <v-container>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-  >
-    <v-layout>
+  <v-container>
+    <v-card
+      class="mx-auto"
+      max-width="1200"
+    >
+      <v-layout>
 
-      <v-app-bar color="#7fffd4">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar color="#7fffd4">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-toolbar-title>My Music</v-toolbar-title>
+          <v-toolbar-title>My Music</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-        <v-btn icon="mdi-magnify"></v-btn>
-      </v-app-bar>
+          <v-btn icon="mdi-magnify"></v-btn>
+        </v-app-bar>
 
-      <v-main>
-        <v-container class="containerCards">
-          <v-row dense>
-            <v-col cols="12">
-              <v-card color="#952175">
-                <div class="d-flex flex-no-wrap justify-space-between">
-                  <div>
-                    <v-card-title class="text-h5">
-                      Halcyon Days
-                    </v-card-title>
-
-                    <v-card-subtitle>Ellie Goulding</v-card-subtitle>
-
-                    <v-card-actions>
-                      <v-btn
-                        class="ms-2"
-                        icon="mdi-play"
-                        variant="text"
-                      ></v-btn>
-                    </v-card-actions>
+        <v-main>
+          <v-container class="containerCards">
+            <v-row dense>
+              <v-col cols="12" md="4" v-for="(radio, index) in radios" :key="index">
+                <v-card :color="radio.color">
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title class="text-h5">
+                        {{ radio.name }}
+                      </v-card-title>
+                      <v-card-subtitle>{{ radio.artist }}</v-card-subtitle>
+                      <v-card-actions>
+                        <v-btn
+                          class="ms-2"
+                          icon="mdi-play"
+                          variant="text"
+                        ></v-btn>
+                      </v-card-actions>
+                    </div>
+                    <v-avatar
+                      class="ma-3"
+                      rounded="0"
+                      size="125"
+                    >
+                      <v-img :src="radio.imageUrl"></v-img>
+                    </v-avatar>
                   </div>
-
-                  <v-avatar
-                    class="ma-3"
-                    rounded="0"
-                    size="125"
-                  >
-                    <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
-                  </v-avatar>
-                </div>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-layout>
-  </v-card>
- </v-container>
-
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-main>
+      </v-layout>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'HomeView',
   data() {
     return {
       radios: [],
-
     }
   },
   methods: {
@@ -72,14 +66,14 @@ export default {
       fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=100&countrycode=IT&hidebroken=true&order=clickcount&reverse=true')
         .then(response => response.json())
         .then(data => {
-          this.radios = data;
-          console.log(data);
+          this.radios = data.map(radio => ({
+            name: radio.name,
+            artist: radio.artist,
+            imageUrl: radio.imageUrl,
+            color: '#952175' // Imposta il colore della card come preferisci
+          }));
+          console.log(this.radios);
         });
-    },
-    generateCard()
-    {
-      //a function that generates cards
-      
     },
   },
   created() {
